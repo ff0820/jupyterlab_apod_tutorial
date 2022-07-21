caaebc318a5fe4afb938dd2b2a0dcdf864573b3c
ff0820 <44333324+ff0820@users.noreply.github.com>
2022-07-21 18:16:54 +0800

    Do with one slide
    1. add the UI: slide control, slide content(card with tags)

--- src/slide-control.tsx
+++ src/slide-control.tsx
@@ -2,10 +2,19 @@ import React, { useEffect, useState, Component } from 'react';
 
 import * as d3 from 'd3';
 import * as _ from 'lodash';
-import { Input, Select, Card, Tree, Button, Space, Switch } from 'antd';
+import {
+  Input,
+  Select,
+  Card,
+  Tree,
+  Slider,
+  Button,
+  Space,
+  Tooltip
+} from 'antd';
 import type { DataNode } from 'antd/es/tree';
 import '../style/index.css'; // active the antd style
-import { SmileOutlined, DownOutlined } from '@ant-design/icons';
+import { DownOutlined, PlusOutlined } from '@ant-design/icons';
 
 export const ControlPanel = (props: any): JSX.Element => {
   // const [counter, setCounter] = useState(0);
@@ -14,15 +23,60 @@ export const ControlPanel = (props: any): JSX.Element => {
   return (
     <div className="control-panel">
       <Space align="center" direction="vertical" size="middle">
+        <ContentViewTest />
         <MetaView onMetaChange={props.onMetaChange} />
         <ContentView contentsStructure={props.contentsStructure} />
-        <ContentViewTest />
+        <SlideControlPanel />
       </Space>
     </div>
   );
 };
 
-export const MetaView = (props: any): JSX.Element => {
+class ContentViewTest extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = { title: 'begin' };
+
+    this.handleTitleEnter = this.handleTitleEnter.bind(this);
+  }
+
+  get contentsStructure() {
+    // if the params used to compute new value is changed,
+    // the computed param will change acoordingly
+    let temp = this.state.title + ' ff is testing.';
+    // console.log('temp', temp);
+    return temp;
+  }
+
+  handleTitleEnter(e) {
+    this.setState({ title: e.target.value });
+    console.log('value', e.target.value);
+  }
+
+  render(): JSX.Element {
+    return (
+      <div className="metaview">
+        <Card bodyStyle={{ padding: '8px' }}>
+          <Space align="start" direction="vertical" size="small">
+            <Space>
+              Author
+              <Input
+                placeholder="input author name"
+                size="small"
+                onPressEnter={this.handleTitleEnter}
+              />
+            </Space>
+            {this.contentsStructure + ''}
+          </Space>
+        </Card>
+      </div>
+    );
+  }
+}
+
+// Do with slides
+const MetaView = (props: any): JSX.Element => {
   // export function MetaView(props: any) {
   let title = '';
   let author = '';
@@ -49,7 +103,7 @@ export const MetaView = (props: any): JSX.Element => {
     const { Option } = Select;
     return (
       <div className="metaview">
-        <Card bodyStyle={{ padding: '8px' }}>
+        <Card size="small" title="Metadata" bodyStyle={{ padding: '8px' }}>
           <Space align="start" direction="vertical" size="small">
             <Space>
               Title &nbsp; &nbsp;
@@ -88,7 +142,7 @@ export const MetaView = (props: any): JSX.Element => {
   return render();
 };
 
-export const ContentView = (props: any): JSX.Element => {
+const ContentView = (props: any): JSX.Element => {
   console.log('props.contentsStructure', props.contentsStructure);
   const defaultData: DataNode[] = props.contentsStructure;
 
@@ -169,8 +223,8 @@ export const ContentView = (props: any): JSX.Element => {
   };
 
   return (
-    <div className="metaview">
-      <Card bodyStyle={{ padding: '8px' }}>
+    <div>
+      <Card size="small" title="Contents" bodyStyle={{ padding: '8px' }}>
         <Space align="start" direction="vertical" size="small">
           <Space>
             Update
@@ -197,45 +251,185 @@ export const ContentView = (props: any): JSX.Element => {
   );
 };
 
-export class ContentViewTest extends Component<any, any> {
+// Do with one slide
+class SlideControlPanel extends Component<any, any> {
   constructor(props: any) {
     super(props);
-
-    this.state = { title: 'begin' };
-
-    this.handleTitleEnter = this.handleTitleEnter.bind(this);
-  }
-
-  get contentsStructure() {
-    // if the params used to compute new value is changed,
-    // the computed param will change acoordingly
-    let temp = this.state.title + ' ff is testing.';
-    // console.log('temp', temp);
-    return temp;
-  }
-
-  handleTitleEnter(e) {
-    this.setState({ title: e.target.value });
-    console.log('value', e.target.value);
   }
 
   render(): JSX.Element {
     return (
-      <div className="metaview">
-        <Card bodyStyle={{ padding: '8px' }}>
+      <div>
+        <Space align="center" direction="vertical" size="middle">
+          <SlideControlView />
+          <SlideContentView />
+        </Space>
+      </div>
+    );
+  }
+}
+
+class SlideControlView extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      tag: '',
+      title: '',
+      contraint: { audienceLevel: 0, detailLevel: 0 }
+    };
+
+    this.handleTagEnter = this.handleTagEnter.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleAudienceLevelChange = this.handleAudienceLevelChange.bind(this);
+    this.handleDetailLevelChange = this.handleDetailLevelChange.bind(this);
+  }
+
+  handleTagEnter(e) {
+    console.log('value', e.target.value);
+    this.setState({ tag: e.target.value });
+  }
+
+  handleTitleChange(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  handleAudienceLevelChange(value: number) {
+    this.setState({
+      constraint: {
+        audienceLevel: value
+      }
+    });
+  }
+
+  handleDetailLevelChange(value: number) {
+    this.setState({
+      constraint: {
+        detailLevel: value
+      }
+    });
+  }
+
+  render(): JSX.Element {
+    const { Option } = Select;
+
+    return (
+      <div>
+        <Card
+          size="small"
+          title="Slide Control"
+          bodyStyle={{ padding: '8px' }}
+          extra={
+            <Tooltip title="add slide">
+              <Button
+                type="primary"
+                shape="circle"
+                icon={<PlusOutlined />}
+                size="small"
+              />
+            </Tooltip>
+          }
+        >
           <Space align="start" direction="vertical" size="small">
             <Space>
-              Author
+              Tag&nbsp;
               <Input
-                placeholder="input author name"
+                placeholder="input tag"
                 size="small"
-                onPressEnter={this.handleTitleEnter}
+                onPressEnter={this.handleTagEnter}
               />
             </Space>
-            {this.contentsStructure + ''}
+            <Space>
+              Title
+              <Select
+                placeholder="select theme"
+                size="small"
+                showArrow
+                onChange={this.handleTitleChange}
+              >
+                <Option value="t1">t1</Option>
+                <Option value="t2">t2</Option>
+                <Option value="t3">t3</Option>
+              </Select>
+            </Space>
+            <Space>
+              Audience
+              <Slider
+                defaultValue={5}
+                min={0}
+                max={5}
+                style={{ width: 100 }}
+                onChange={this.handleAudienceLevelChange}
+              />
+            </Space>
+            <Space>
+              Detail &nbsp; &nbsp; &nbsp;
+              <Slider
+                defaultValue={0}
+                min={0}
+                max={5}
+                style={{ width: 100 }}
+                onChange={this.handleDetailLevelChange}
+              />
+            </Space>
+            <Space
+              direction="vertical"
+              size="middle"
+              style={{ display: 'flex', width: 220 }}
+            >
+              <Button type="primary" size="small">
+                Generate
+              </Button>
+            </Space>
           </Space>
         </Card>
       </div>
     );
   }
 }
+
+const SlideContentView: React.FC = (props: any): JSX.Element => {
+  const tabListNoTitle = [
+    {
+      key: 'article',
+      tab: 'article'
+    },
+    {
+      key: 'app',
+      tab: 'app'
+    },
+    {
+      key: 'project',
+      tab: 'project'
+    }
+  ];
+
+  const contentListNoTitle: Record<string, React.ReactNode> = {
+    article: <p>replace with my component</p>,
+    app: <p>app content</p>,
+    project: <p>project content</p>
+  };
+
+  const [activeTabKey2, setActiveTabKey2] = useState<string>('article');
+
+  const onTab2Change = (key: string) => {
+    setActiveTabKey2(key);
+  };
+
+  return (
+    <>
+      <Card
+        size="small"
+        bodyStyle={{ padding: 8 }}
+        style={{ width: '100%' }}
+        tabList={tabListNoTitle}
+        activeTabKey={activeTabKey2}
+        onTabChange={key => {
+          onTab2Change(key);
+        }}
+      >
+        {contentListNoTitle[activeTabKey2]}
+      </Card>
+    </>
+  );
+};
